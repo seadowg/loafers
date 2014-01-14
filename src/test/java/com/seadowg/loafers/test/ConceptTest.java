4c0ef612b345daf54ff574bd93961d53a5fdb248
Callum Stott <callum@seadowg.com>
2014-01-14 00:43:42 +0000

    Wow such tests

---
+++ src/test/java/com/seadowg/loafers/test/ConceptTest.java
@@ -0,0 +1,57 @@
+package com.seadowg.loafers.test;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import com.seadowg.loafers.app.App;
+import com.seadowg.loafers.collection.List;
+import com.seadowg.loafers.widget.Button;
+import com.seadowg.loafers.widget.Input;
+import com.seadowg.loafers.widget.Popup;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.shadows.ShadowDialog;
+
+import static junit.framework.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import static org.robolectric.Robolectric.shadowOf;
+
+@RunWith(RobolectricTestRunner.class)
+public class ConceptTest {
+  @Test
+  public void buildingAMagic8Ball_works() throws Exception {
+    Activity app = Robolectric.buildActivity(MyApp.class).create().start().resume().get();
+
+    FrameLayout rootView = (FrameLayout) app.getWindow().getDecorView().getRootView();
+    LinearLayout rootLayout = (LinearLayout) rootView.getChildAt(0);
+    LinearLayout layout = ((LinearLayout) ((FrameLayout) rootLayout.getChildAt(2)).getChildAt(0));
+
+    android.widget.EditText editText = (EditText) layout.getChildAt(0);
+    android.widget.Button button = (android.widget.Button) layout.getChildAt(1);
+
+    assertNotNull(editText);
+    assertNotNull(button);
+
+    button.performClick();
+    AlertDialog popup = (AlertDialog) ShadowDialog.getLatestDialog();
+    assertEquals("Yes!", shadowOf(popup).getTitle());
+  }
+
+  public static class MyApp extends App {
+    public void open() {
+      title = "Magic 8-Ball";
+
+      new Input();
+      new Button("Shake") {
+        public void click() {
+          String answer = new List("Yes!").chooseOne();
+          new Popup(answer).show();
+        }
+      };
+    }
+  }
+}
